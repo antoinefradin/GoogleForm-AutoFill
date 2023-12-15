e3b3071d34cccdabb237e27dd5e56a2cf22709bf
Antoine Fradin <fradin.antoine@yahoo.fr>
2023-12-15 19:21:08 +0100

    first commit

---
+++ AutoFill.py
@@ -0,0 +1,55 @@
+# Autofill Google Form
+import requests
+import time
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+GCP_PROJECT_ID = os.getenv('GCP_PROJECT_ID')
+
+# URL to the form you want to fill. formResponse should be used instead of viewform
+url = os.getenv('url')
+
+
+def get_values(course,promo,cursus,project):
+    """It returns a dict of different form data to be submitted by send_form method."""
+
+    values = {
+        "entry.23904865" : os.getenv('name'),
+        "entry.1480848492" : project,
+        "entry.1770104696" : os.getenv('mail'),
+        "entry.1277129807" : promo,
+        "entry.1677673843" : course,
+        "entry.615985793" : "Non - No",
+        "entry.1027757300" : cursus,
+        "entry.555479852" : "FR",
+        "entry.78623536" : "Non - No",
+    }
+
+    return values
+
+
+
+def send_forms(url, values):
+    """It takes google form url and the data which is a dict of key/value."""
+    try:
+        r = requests.post(url, data=values)
+        print("\t\t",r,"\n")
+        time.sleep(20)
+    except:
+        print("Error Occured!")
+    return r
+
+
+for course in ['DE']:
+    for promo in ['JAN24','FEB24','MAR24','APR24','MAY24','JUN24','JUL24','AUG24']:
+        for cursus in ['Bootcamp - Full-time', 'Continu - Part-time']:
+            for project in ['NY News', 'Satisfaction client', 'OPA', 'Job market', 'Ecobalyse', 'DST Airlines']:
+
+                final_data = get_values(course,promo,cursus,project)
+                
+                print(course,promo,cursus,"---",project)
+                r = send_forms(url, final_data)
+
+
---
+++ README.md
@@ -0,0 +1,73 @@
+# Google Form AutoFill
+Automated the Google Form filling.
+
+Python script build with Requests that fills data to Google forms in an infinite loop .
+
+
+&nbsp; 
+
+
+
+## ⚙️ **Prerequisites and Installation**
+
+To start working with the **Google Form AutoFill** script, you need to set up your Python environment.  
+  
+In summary, this is what you will need:
+- Python 3
+- virtualenv
+
+&nbsp; 
+
+```bash
+$ pip3 install virtualenv
+
+#Test your installation:
+$ virtualenv --version
+
+# Create your virtual environment
+$ python3 -m venv .venv
+
+# you can activate the virtual environment by just input
+source .venv/bin/activate
+
+# then you can deactivate it by just type in deactivate.
+source .venv/bin/deactivate
+
+# If you want to delete that virtual env, simply do
+rm -rf .venv
+```
+
+&nbsp; 
+
+Once the `virtualenv` is activated, you can install the required dependencies.
+
+```bash
+$ pip3 install -r requirements.txt
+```
+
+&nbsp; 
+
+
+## 🚀 **Starting**
+
+Launch the python script using: 
+```
+$ python3 AutoFill.py 
+````
+
+> ***Note:*** Edit your form's details inside the script.
+>
+> You can cancel the loop by using ``Control+Z`` while statement.
+
+ 
+&nbsp; 
+
+&nbsp; 
+
+&nbsp; 
+
+&nbsp; 
+
+## **Sources:**
+- GoogleForm-AutoFill: https://github.com/jamesshah/GoogleForm-AutoFill
+
