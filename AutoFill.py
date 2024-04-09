2c96d556adc79ea8864689cfb4e7759e97180d94
Antoine Fradin <fradin.antoine@yahoo.fr>
2024-04-09 16:45:35 +0200

    april 24

--- AutoFill.py
+++ AutoFill.py
@@ -18,12 +18,12 @@ def get_values(course,promo,cursus,project):
     values = {
         "entry.23904865" : os.getenv('name'),
         "entry.1480848492" : project,
-        "entry.1770104696" : os.getenv('mail'),
+        "entry.1770104696" : str(os.getenv('mail')) + "(Jusqu'à 10h disponibles par semaine, 10 groupes / mois)",
         "entry.1277129807" : promo,
         "entry.1677673843" : course,
         "entry.615985793" : "Non - No",
         "entry.1027757300" : cursus,
-        "entry.555479852" : "FR",
+        "entry.555479852" : "FR uniquement",
         "entry.78623536" : "Non - No",
     }
 
@@ -36,20 +36,25 @@ def send_forms(url, values):
     try:
         r = requests.post(url, data=values)
         print("\t\t",r,"\n")
-        time.sleep(20)
+        time.sleep(30)
     except:
         print("Error Occured!")
     return r
 
 
+
 for course in ['DE']:
-    for promo in ['JAN24','FEB24','MAR24','APR24','MAY24','JUN24','JUL24','AUG24']:
-        for cursus in ['Bootcamp - Full-time', 'Continu - Part-time']:
-            for project in ['NY News', 'Satisfaction client', 'OPA', 'Job market', 'Ecobalyse', 'DST Airlines']:
+    for promo in ['APR24','MAY24']:#,'JUN24','JUL24','AUG24']: # ['DEC23','JAN24','FEB24','MAR24','APR24','MAY24','JUN24','JUL24','AUG24']:
+        for cursus in ['Continu - Part-time','Bootcamp - Full-time']: #'Bootcamp - Full-time'
+            for project in ['NY News', 'Satisfaction client', 'OPA', 'Job market', 'DST Airlines']:
+            #for project in ['Recommendation de films', 'Prévision météo Australie', 'Classification de produits e-commerce Rakuten',
+            # 'Accidents routiers en France', 'Recommendation de musiques', 'Paris sportifs']:
 
                 final_data = get_values(course,promo,cursus,project)
                 
                 print(course,promo,cursus,"---",project)
                 r = send_forms(url, final_data)
+
+
 
 
