5ca58c718d2c388128f62b9493bcdf294549cb17
Antoine Fradin <fradin.antoine@yahoo.fr>
2025-04-01 19:16:28 +0200

    add the Devops option

--- AutoFill.py
+++ AutoFill.py
@@ -20,12 +20,12 @@ def get_values(course,promo,cursus,project):
     values = {
         "entry.23904865" : os.getenv('name'),
         "entry.1480848492" : project,
-        "entry.1770104696" : str(os.getenv('mail')) + "   (Beaucoup de disponibilités à partir du 1er Janvier 2025)",
+        "entry.1770104696" : str(os.getenv('mail')) + "   (Beaucoup de disponibilités à partir du 1er Avril 2025)",
         "entry.1277129807" : promo,
         "entry.1677673843" : course,
         "entry.615985793" : "Oui - Yes",
         "entry.1027757300" : cursus,
-        "entry.555479852" : "FR or EN", #"FR uniquement",
+        "entry.555479852" : "FR uniquement", #"FR or EN", 
         #"entry.78623536" : "Non - No",
     }
 
@@ -48,13 +48,14 @@ send_de = True
 send_mlops = True
 send_ds = True
 send_da = True
+send_devops = False # New! 
 
 
 if send_de:
     for course in ['DE']:
-        for promo in ["JAN25","FEB25"]: # ['DEC23','JAN24','FEB24','MAR24','APR24','MAY24','JUN24','JUL24','AUG24']:
+        for promo in ["MAR25","AVR25"]: # ['DEC23','JAN24','FEB24','MAR24','APR24','MAY24','JUN24','JUL24','AUG24']:
             for cursus in ['Continu - Part-time','Bootcamp - Full-time']: #'Bootcamp - Full-time'
-                for project in ['NY News', 'Satisfaction client', 'OPA', 'Job market', 'DST Airlines',"Itinéraire de vacances"]:
+                for project in ['Satisfaction client', 'Crypto / OPA', 'Job market', 'DST Airlines',"Itinéraire de vacances", "Autre"]:
 
                     final_data = get_values(course,promo,cursus,project)
                     
@@ -64,10 +65,10 @@ if send_de:
 
 if send_ds:
     for course in ['DS']:
-        for promo in ["JAN25","FEB25"]: 
+        for promo in ["MAR25","AVR25"]: 
             for cursus in ['Continu - Part-time','Bootcamp - Full-time']:
                 for project in ["Système de recommendation de films", "Prévision météo Australie", 'Classification de produits e-commerce Rakuten',
-                 'Emission de CO2 par les véhicules', 'Recommendation de plantes', 'Analyse des tirs de joueurs NBA']:
+                 'Emission de CO2 par les véhicules', 'Supply Chain - Satisfaction des clients', 'Recommendation de plantes', 'Analyse des tirs de joueurs NBA', "Autre"]:
 
                     final_data = get_values(course,promo,cursus,project)
                     
@@ -76,9 +77,9 @@ if send_ds:
 
 if send_mlops:
     for course in ['MLOps']:
-        for promo in ["JAN25","FEB25"]: 
+        for promo in ["MAR25","AVR25"]: 
             for cursus in ['Continu - Part-time','Bootcamp - Full-time']:
-                for project in ["Recommendation de films", "Prévision météo Australie", 'Classification de produits e-commerce Rakuten',
+                for project in ["Système de recommendation de films", "Prévision météo Australie", 'Classification de produits e-commerce Rakuten',
                    'Recommendation de musiques', 'Paris sportif',"Accidents routiers en France"]:
 
                     final_data = get_values(course,promo,cursus,project)
@@ -89,7 +90,18 @@ if send_mlops:
 
 if send_da:
     for course in ['DA']:
-        for promo in ["JAN25","FEB25"]: 
+        for promo in ["MAR25","AVR25"]: 
+            for cursus in ['Continu - Part-time','Bootcamp - Full-time']: 
+                for project in ["Data job", "Analyse des tirs de joueurs NBA", "Energie", "Recommendation de films", "Prédire le succès d\'une campagne de financement participatif", "Mon Petit Gazon","Trafic cycliste à Paris"]:
+
+                    final_data = get_values(course,promo,cursus,project)
+                    
+                    print(course,promo,cursus,"---",project)
+                    r = send_forms(url, final_data)
+
+if send_devops:
+    for course in ['DevOps']:
+        for promo in ["MAR25","AVR25"]: 
             for cursus in ['Continu - Part-time','Bootcamp - Full-time']: 
                 for project in ["Data job", "Analyse des tirs de joueurs NBA", "Energie", "Recommendation de films", "Prédire le succès d\'une campagne de financement participatif", "Mon Petit Gazon","Trafic cycliste à Paris"]:
 
